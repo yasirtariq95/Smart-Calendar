4ad820c5e2ee366b9fc7d5d56c05b76d0343677e
jinhuh229 <37564888+jinhuh229@users.noreply.github.com>
2018-11-07 21:17:20 -0500

    Update AddEvent.java

--- AddEvent.java
+++ AddEvent.java
@@ -55,6 +55,7 @@ public class AddEvent {
                 frame.getContentPane().setBackground(new Color(0, 51, 102));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+                frame.setTitle("Add Event");
 		
      
                  
@@ -81,23 +82,53 @@ public class AddEvent {
                 
                 JLabel Start = new JLabel("Start");
 		Start.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
-		Start.setBounds(47, 300, 95, 20);
+		Start.setBounds(47, 260, 95, 20);
                  Start.setForeground(new java.awt.Color(255, 255, 255));
 		frame.getContentPane().add(Start);
+
+                JLabel Start1 = new JLabel("(Month, Day, Time)");
+		Start1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
+		Start1.setBounds(700, 260, 150, 20);
+                 Start1.setForeground(new java.awt.Color(255, 255, 255));
+		frame.getContentPane().add(Start1);
                 
-                JFormattedTextField eventStart = new JFormattedTextField();
-		eventStart.setBounds(182, 300, 270, 40);
-		frame.getContentPane().add(eventStart);
+                
+                JFormattedTextField eventStartMonth = new JFormattedTextField();
+		eventStartMonth.setBounds(182, 260, 100, 30);
+		frame.getContentPane().add(eventStartMonth);
+                
+                JFormattedTextField eventStartDay = new JFormattedTextField();
+		eventStartDay.setBounds(382, 260, 100, 30);
+		frame.getContentPane().add(eventStartDay);
+                
+                JFormattedTextField eventStartTime = new JFormattedTextField();
+		eventStartTime.setBounds(582, 260, 100, 30);
+		frame.getContentPane().add(eventStartTime);
                 
                 JLabel End = new JLabel("End");
 		End.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
-		End.setBounds(500, 300, 95, 20);
+		End.setBounds(47, 330, 95, 20);
                 End.setForeground(new java.awt.Color(255, 255, 255));
 		frame.getContentPane().add(End);
                 
-                JFormattedTextField eventEnd = new JFormattedTextField();
-		eventEnd.setBounds(600, 300,270, 40);
-		frame.getContentPane().add(eventEnd);
+                JLabel End1 = new JLabel("(Month, Day, Time)");
+		End1.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
+		End1.setBounds(700, 330, 150, 20);
+                End1.setForeground(new java.awt.Color(255, 255, 255));
+		frame.getContentPane().add(End1);
+                
+                JFormattedTextField eventEndMonth = new JFormattedTextField();
+		eventEndMonth.setBounds(182, 330,100, 30);
+		frame.getContentPane().add(eventEndMonth);
+                
+                JFormattedTextField eventEndDay = new JFormattedTextField();
+		eventEndDay.setBounds(382, 330,100, 30);
+		frame.getContentPane().add(eventEndDay);
+                
+                
+                JFormattedTextField eventEndTime = new JFormattedTextField();
+		eventEndTime.setBounds(582, 330,100, 30);
+		frame.getContentPane().add(eventEndTime);
                 
                 
                 JLabel Location = new JLabel("Location");
@@ -107,19 +138,19 @@ public class AddEvent {
 		frame.getContentPane().add(Location);
                 
                 JFormattedTextField eventLocation = new JFormattedTextField();
-		eventLocation.setBounds(182, 400, 700, 73);
+		eventLocation.setBounds(182, 420, 700, 73);
 		frame.getContentPane().add(eventLocation);
                 
                 
                 
                 JLabel lblDescription = new JLabel("Description");
 		lblDescription.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
-		lblDescription.setBounds(47, 520, 95, 20);
+		lblDescription.setBounds(47, 530, 95, 20);
                 lblDescription.setForeground(new java.awt.Color(255, 255, 255));
 		frame.getContentPane().add(lblDescription);
               
 		JFormattedTextField eventDescr = new JFormattedTextField();
-		eventDescr.setBounds(182, 500, 700, 73);
+		eventDescr.setBounds(182, 530, 700, 90);
 		frame.getContentPane().add(eventDescr);
 		
 
@@ -134,8 +165,20 @@ public class AddEvent {
                            //event.EventMain();
 			}
 		});
-		btnAdd.setBounds(150, 80, 140, 42);
+		btnAdd.setBounds(180, 80, 140, 42);
 		frame.getContentPane().add(btnAdd);
+                
+                JButton btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Berlin Sans FB", Font.PLAIN, 14));
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+                            
+                           //EventMain event = new EventMain();
+                           //event.EventMain();
+			}
+		});
+		btnDelete.setBounds(380, 80, 140, 42);
+		frame.getContentPane().add(btnDelete);
                 
                 
 		JButton btnBack = new JButton("Back");
@@ -148,7 +191,7 @@ public class AddEvent {
                 frame.dispose();
 			}
 		});
-		btnBack.setBounds(350, 80, 140, 42);
+		btnBack.setBounds(580, 80, 140, 42);
 		frame.getContentPane().add(btnBack);
                 
          
@@ -158,12 +201,12 @@ public class AddEvent {
 		eventReservation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
                             
-                           EventMain event = new EventMain();
+                          EventMain event  = new EventMain();
                            event.EventMain();
                            
 			}
 		});
-		eventReservation.setBounds(550, 80, 140, 42);
+		eventReservation.setBounds(200, 650, 140, 42);
 		frame.getContentPane().add(eventReservation);
                 
                 
@@ -177,7 +220,7 @@ public class AddEvent {
                            
 			}
 		});
-		ContactList.setBounds(750, 80, 140, 42);
+		ContactList.setBounds(400, 650, 140, 42);
 		frame.getContentPane().add(ContactList);
                 
 	}
